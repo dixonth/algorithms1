d0cb4b8c5d5eac99a8e865065d739cf76ca2af94
Thomas Joseph Dixon <dixonth@tcd.ie>
2020-02-25 19:28:15 +0000

    Added tests for all sorting algorithms

--- src/SortComparison.java
+++ src/SortComparison.java
@@ -18,11 +18,12 @@
      *
      */
     static double [] insertionSort (double a[]){
+    	if (a ==null) return null;
     	int n = a.length; 
         for (int i = 1; i < n; ++i) { 
             double key = a[i]; 
             int j = i - 1;
-            /* Move elements of arr[0..i-1], that are 
+            /* Move elements of a[0..i-1], that are 
                greater than key, to one position ahead 
                of their current position */
             while (j >= 0 && a[j] > key) { 
@@ -42,6 +43,7 @@
      *
      */
     static double [] selectionSort (double a[]){
+    	if (a ==null) return null;
     	int n = a.length; 
     	  
         // One by one move boundary of unsorted subarray 
@@ -70,11 +72,58 @@
      *
      */
     static double [] quickSort (double a[]){
+    	if (a ==null) return null;
+		qSort(a, 0, a.length-1);
 		return a;
-	
 		 //todo: implement the sort
 
     }//end quicksort
+    
+    static int partition(double a[], int low, int high) 
+    { 
+        double pivot = a[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than the pivot 
+            if (a[j] < pivot) 
+            { 
+                i++; 
+  
+                // swap a[i] and a[j] 
+                double temp = a[i]; 
+                a[i] = a[j]; 
+                a[j] = temp; 
+            } 
+        } 
+  
+        // swap a[i+1] and a[high] (or pivot) 
+        double temp = a[i+1]; 
+        a[i+1] = a[high]; 
+        a[high] = temp; 
+  
+        return i+1; 
+    } 
+  
+  
+    /* The main function that implements QuickSort() 
+      a[] --> array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    static void qSort(double a[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            /* pi is partitioning index, a[pi] is  
+              now at right place */
+            int pi = partition(a, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            qSort(a, low, pi-1); 
+            qSort(a, pi+1, high); 
+        } 
+    } 
 
     /**
      * Sorts an array of doubles using Merge Sort.
@@ -92,12 +141,105 @@
      */
 
     static double[] mergeSortIterative (double a[]) {
-		return a;
-
-		 //todo: implement the sort
-	
+    	if (a ==null) return null;
+    	// For current size of subarrays to 
+        // be merged curr_size varies from  
+        // 1 to n/2 
+    	int n =a.length;
+        int curr_size;  
+                      
+        // For picking starting index of  
+        // left subarray to be merged 
+        int left_start; 
+                          
+          
+        // Merge subarrays in bottom up  
+        // manner. First merge subarrays  
+        // of size 1 to create sorted  
+        // subarrays of size 2, then merge 
+        // subarrays of size 2 to create  
+        // sorted subarrays of size 4, and 
+        // so on. 
+        for (curr_size = 1; curr_size <= n-1;  
+                      curr_size = 2*curr_size) 
+        { 
+              
+            // Pick starting point of different 
+            // subarrays of current size 
+            for (left_start = 0; left_start < n-1; 
+                        left_start += 2*curr_size) 
+            { 
+                // Find ending point of left  
+                // subarray. mid+1 is starting  
+                // point of right 
+                int mid = Math.min(left_start + curr_size - 1, n-1); 
+          
+                int right_end = Math.min(left_start  
+                             + 2*curr_size - 1, n-1); 
+          
+                // Merge Subarrays arr[left_start...mid] 
+                // & arr[mid+1...right_end] 
+                merge(a, left_start, mid, right_end); 
+            } 
+        } 
+        return a;
     }//end mergesortIterative
     
+    static void merge(double a[], int l, int m, int r) 
+    { 
+        int i, j, k; 
+        int n1 = m - l + 1; 
+        int n2 = r - m; 
+      
+        /* create temp arrays */
+        double L[] = new double[n1]; 
+        double R[] = new double[n2]; 
+      
+        /* Copy data to temp arrays L[] 
+        and R[] */
+        for (i = 0; i < n1; i++) 
+            L[i] = a[l + i]; 
+        for (j = 0; j < n2; j++) 
+            R[j] = a[m + 1+ j]; 
+      
+        /* Merge the temp arrays back into 
+        arr[l..r]*/
+        i = 0; 
+        j = 0; 
+        k = l; 
+        while (i < n1 && j < n2) 
+        { 
+            if (L[i] <= R[j]) 
+            { 
+                a[k] = L[i]; 
+                i++; 
+            } 
+            else
+            { 
+                a[k] = R[j]; 
+                j++; 
+            } 
+            k++; 
+        } 
+      
+        /* Copy the remaining elements of  
+        L[], if there are any */
+        while (i < n1) 
+        { 
+            a[k] = L[i]; 
+            i++; 
+            k++; 
+        } 
+      
+        /* Copy the remaining elements of 
+        R[], if there are any */
+        while (j < n2) 
+        { 
+            a[k] = R[j]; 
+            j++; 
+            k++; 
+        } 
+    } 
     
     
     /**
@@ -108,11 +250,65 @@
      * @return after the method returns, the array must be in ascending sorted order.
      */
     static double[] mergeSortRecursive (double a[]) {
-		return a;
+    	if(a == null) 
+        { 
+            return a; 
+        } 
     	
-
-    	//todo: implement the sort
-	
+    	if(a.length > 1) 
+        { 
+            int mid = a.length / 2; 
+  
+            // Split left part 
+            double[] left = new double[mid]; 
+            for(int i = 0; i < mid; i++) 
+            { 
+                left[i] = a[i]; 
+            } 
+              
+            // Split right part 
+            double[] right = new double[a.length - mid]; 
+            for(int i = mid; i < a.length; i++) 
+            { 
+                right[i - mid] = a[i]; 
+            } 
+            mergeSortRecursive(left); 
+            mergeSortRecursive(right); 
+  
+            int i = 0; 
+            int j = 0; 
+            int k = 0; 
+  
+            // Merge left and right arrays 
+            while(i < left.length && j < right.length) 
+            { 
+                if(left[i] < right[j]) 
+                { 
+                    a[k] = left[i]; 
+                    i++; 
+                } 
+                else
+                { 
+                    a[k] = right[j]; 
+                    j++; 
+                } 
+                k++; 
+            } 
+            // Collect remaining elements 
+            while(i < left.length) 
+            { 
+                a[k] = left[i]; 
+                i++; 
+                k++; 
+            } 
+            while(j < right.length) 
+            { 
+                a[k] = right[j]; 
+                j++; 
+                k++; 
+            } 
+        } 
+    	return a;
    }//end mergeSortRecursive
     	
     
@@ -121,9 +317,8 @@
    
 
 
-    public static void main(String[] args) {
-
-        //todo: do experiments as per assignment instructions
-    }
+//    public static void main(String[] args) {
+//        //todo: do experiments as per assignment instructions
+//    }
 
  }//end class
--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -1,5 +1,5 @@
 import static org.junit.Assert.assertEquals;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -24,19 +24,80 @@ public class SortComparisonTest
     //~ Public Methods ........................................................
 
     // ----------------------------------------------------------
-    /**
-     * Check that the methods work for empty arrays
-     */
+    
+    
     @Test
-    public void testEmpty()
+    public void insertionSort()
     {
+    	double [] inArray = {4,7,1,6,3,8,2,5};
+    	double [] outArray = {1,2,3,4,5,6,7,8};
+    	
+    	double [] emptyArray = {};
+    	double [] nullArray = null;
+
+        Assert.assertArrayEquals("insertionSort with normal array", outArray, SortComparison.insertionSort(inArray), 0);
+        Assert.assertArrayEquals("insertionSort with empty array", emptyArray, SortComparison.insertionSort(emptyArray), 0);
+        Assert.assertArrayEquals("insertionSort with null array", null, SortComparison.insertionSort(nullArray), 0);
     }
 
 
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
-    // be executed at least once from at least one test.
+    @Test
+    public void selectionSort()
+    {
+    	double [] inArray = {4,7,1,6,3,8,2,5};
+    	double [] outArray = {1,2,3,4,5,6,7,8};
+    	
+    	double [] emptyArray = {};
+    	double [] nullArray = null;
 
-    // ----------------------------------------------------------
+        Assert.assertArrayEquals("selectionSort with normal array", outArray, SortComparison.selectionSort(inArray), 0);
+        Assert.assertArrayEquals("selectionSort with empty array", emptyArray, SortComparison.selectionSort(emptyArray), 0);
+        Assert.assertArrayEquals("selectionSort with null array", null, SortComparison.selectionSort(nullArray), 0);
+    }
+    
+    @Test
+    public void quickSort()
+    {
+    	double [] inArray = {4,7,1,6,3,8,2,5};
+    	double [] outArray = {1,2,3,4,5,6,7,8};
+    	
+    	double [] emptyArray = {};
+    	double [] nullArray = null;
+
+        Assert.assertArrayEquals("quickSort with normal array", outArray, SortComparison.quickSort(inArray), 0);
+        Assert.assertArrayEquals("quickSort with empty array", emptyArray, SortComparison.quickSort(emptyArray), 0);
+        Assert.assertArrayEquals("quickSort with null array", null, SortComparison.quickSort(nullArray), 0);
+    }
+    
+    @Test
+    public void mergeSortIterative()
+    {
+    	double [] inArray = {4,7,1,6,3,8,2,5};
+    	double [] outArray = {1,2,3,4,5,6,7,8};
+    	
+    	double [] emptyArray = {};
+    	double [] nullArray = null;
+
+        Assert.assertArrayEquals("mergeSortIterative with normal array", outArray, SortComparison.mergeSortIterative(inArray), 0);
+        Assert.assertArrayEquals("mergeSortIterative with empty array", emptyArray, SortComparison.mergeSortIterative(emptyArray), 0);
+        Assert.assertArrayEquals("mergeSortIterative with null array", null, SortComparison.mergeSortIterative(nullArray), 0);
+    }
+    
+    @Test
+    public void mergeSortRecursive()
+    {
+    	double [] inArray = {4,7,1,6,3,8,2,5};
+    	double [] outArray = {1,2,3,4,5,6,7,8};
+    	
+    	double [] emptyArray = {};
+    	double [] nullArray = null;
+
+        Assert.assertArrayEquals("mergeSortRecursive with normal array", outArray, SortComparison.mergeSortRecursive(inArray), 0);
+        Assert.assertArrayEquals("mergeSortRecursive with empty array", emptyArray, SortComparison.mergeSortRecursive(emptyArray), 0);
+        Assert.assertArrayEquals("mergeSortRecursive with null array", null, SortComparison.mergeSortRecursive(nullArray), 0);
+    }
+    
+    
     /**
      *  Main Method.
      *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
@@ -45,6 +106,7 @@ public class SortComparisonTest
     public static void main(String[] args)
     {
         //TODO: implement this method
+    	int i =5;
     }
 
 }
